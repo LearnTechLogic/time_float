1784590a867cbee2d656bd182c9f1c3b1f37ecf3
zhaoxinhang <3285922090@qq.com>
2026-02-26 11:34:47 +0800

    基础框架

---
+++ time_float_backend/src/main/java/com/time_float_backend/TimefloatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.time_float_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimefloatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimefloatBackendApplication.class, args);
+    }
+
+}
---
+++ time_float_backend/src/test/java/com/time_float_backend/TimefloatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time_float_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimefloatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
